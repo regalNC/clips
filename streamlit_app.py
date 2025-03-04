15e5e8e317aa629a53ab31097b65fd05d3b6564c
regalNC <37859580+regalNC@users.noreply.github.com>
2025-03-04 10:44:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import clips
 
+env = clips.Environment()
+env.eval("(assert (v 2))")
+
+for i in env.facts():
+    st.write(i)
+
 st.title("🎈 My new app2")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
